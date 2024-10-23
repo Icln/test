33dc85421879b0e8344d09eec752affefd90e348
임강호 <55732952+Icln@users.noreply.github.com>
2024-10-23 20:10:05 +0900

    feat: 초기세팅

---
+++ src/test/java/Lake/Final_PJT/FinalPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package Lake.Final_PJT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
